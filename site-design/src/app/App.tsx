f06db25e769eb06c07b073c0eacef6b7a3e1d91a
jeffry.luque <jeffry.luque@ucr.ac.cr>
2026-08-27 15:25:32 -0600

    Registrar visitas de Meta Pixel en todas las rutas

--- site-design/src/app/App.tsx
+++ site-design/src/app/App.tsx
@@ -1,8 +1,28 @@
 import { RouterProvider } from "react-router";
+import { useEffect } from "react";
 import { router } from "./routes";
 import "../styles/fonts.css";
 import "../styles/theme.css";
 
+declare global {
+  interface Window {
+    fbq?: (...args: unknown[]) => void;
+  }
+}
+
 export default function App() {
+  useEffect(() => {
+    let currentPage = `${router.state.location.pathname}${router.state.location.search}`;
+
+    return router.subscribe((state) => {
+      const nextPage = `${state.location.pathname}${state.location.search}`;
+
+      if (nextPage !== currentPage) {
+        currentPage = nextPage;
+        window.fbq?.("track", "PageView");
+      }
+    });
+  }, []);
+
   return <RouterProvider router={router} />;
 }
